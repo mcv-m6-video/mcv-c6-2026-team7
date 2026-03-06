bc34ea4daa1f13f62edcb6f79640b66e03dae685
Aledonairesa <aledonairesa@gmail.com>
2026-03-06 13:17:09 +0100

    Add RAFT large and small off-the-shelf models

--- Week3/optical_flow/main.py
+++ Week3/optical_flow/main.py
@@ -2,6 +2,7 @@ import cv2
 import time
 import argparse
 import numpy as np
+from torchvision.io import read_image
 
 from .utils import (
     read_kitti_flow,
@@ -35,6 +36,18 @@ def main(args):
         img1 = np.atleast_3d(img1.astype(float) / 255.0)
         img2 = cv2.imread(args.img2_path, cv2.IMREAD_GRAYSCALE)
         img2 = np.atleast_3d(img2.astype(float) / 255.0)
+
+    elif args.model == "raft_large" or args.model == "raft_small":
+        
+        params = []
+        
+        # We use torchvision's read_image
+        img1 = read_image(args.img1_path)  # CxHxW uint8 tensor
+        img2 = read_image(args.img2_path)  # CxHxW uint8 tensor
+
+        # Images are grayscale but RAFT expects 3 channels
+        img1 = img1.repeat(3, 1, 1)  # (3, H, W)
+        img2 = img2.repeat(3, 1, 1)  # (3, H, W)
     
     msen, pepn, runtime = evaluate_model(args.model, params, img1, img2, gt)
 
@@ -47,7 +60,7 @@ def main(args):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', choices=["pyflow"], default="pyflow")
+    parser.add_argument('--model', choices=["pyflow", "raft_large", "raft_small"], default="pyflow")
     parser.add_argument('--gt_path', help="Path to the Ground Truth stereo flow", default="data/data_stereo_flow/training/flow_noc/000045_10.png")
     parser.add_argument('--img1_path', help="Path to the first image.", default="data/data_stereo_flow/training/image_0/000045_10.png")
     parser.add_argument('--img2_path', help="Path to the second image.", default="data/data_stereo_flow/training/image_0/000045_11.png")
--- Week3/optical_flow/utils.py
+++ Week3/optical_flow/utils.py
@@ -1,6 +1,29 @@
+import cv2
 import numpy as np
+import torch
 import pyflow
 import png
+from torchvision.models.optical_flow import Raft_Large_Weights, Raft_Small_Weights, raft_large, raft_small
+from torchvision.transforms.functional import resize
+import torchvision.transforms.functional as F
+
+
+class InputPadder:
+    """Pads images such that dimensions are divisible by 8. Used for RAFT"""
+    def __init__(self, dims):
+        self.ht, self.wd = dims[-2], dims[-1]
+        pad_ht = (((self.ht // 8) + 1) * 8 - self.ht) % 8
+        pad_wd = (((self.wd // 8) + 1) * 8 - self.wd) % 8
+        self._pad = [pad_wd // 2, pad_wd - pad_wd // 2,  # left, right
+                     pad_ht // 2, pad_ht - pad_ht // 2]  # top, bottom
+
+    def pad(self, *imgs):
+        return [torch.nn.functional.pad(img, self._pad, mode='replicate') for img in imgs]
+
+    def unpad(self, x):
+        ht, wd = x.shape[-2], x.shape[-1]
+        c = [self._pad[2], ht - self._pad[3], self._pad[0], wd - self._pad[1]]
+        return x[..., c[0]:c[1], c[2]:c[3]]
 
 
 def read_kitti_flow(flow_file: str) -> np.ndarray:
@@ -28,7 +51,45 @@ def compute_optical_flow(model: str, params: list, img1: np.ndarray, img2: np.nd
             nInnerFPIterations, nSORIterations, colType
         )
 
-    return np.dstack((u, v))
+        return np.dstack((u, v))
+
+    elif model == "raft_large" or model == "raft_small":
+        # Get the default weights and transforms for RAFT
+        if model == "raft_large":
+            weights = Raft_Large_Weights.DEFAULT
+        else:
+            weights = Raft_Small_Weights.DEFAULT
+        transforms = weights.transforms()
+
+        # Apply RAFT transforms
+        img1, img2 = transforms(img1.unsqueeze(0), img2.unsqueeze(0))
+
+        # Pad to make dimensions divisible by 8
+        padder = InputPadder(img1.shape)
+        img1, img2 = padder.pad(img1, img2)
+
+        # Select device
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        # Load the RAFT model
+        if model == "raft_large":
+            model = raft_large(weights=Raft_Large_Weights.DEFAULT, progress=False).to(device)
+        elif model == "raft_small":
+            model = raft_small(weights=Raft_Small_Weights.DEFAULT, progress=False).to(device)
+        model = model.eval()
+
+        # RAFT outputs lists of predicted flows. Each entry is a (N, 2, H, W) batch of predicted
+        # flows that corresponds to a given iteration. We take the last one as our final prediction.
+        list_of_flows = model(img1.to(device), img2.to(device))
+        predicted_flows = list_of_flows[-1]
+
+        # Unpad the flow back to original resolution
+        predicted_flows = padder.unpad(predicted_flows)
+
+        # Convert the predicted flow to a numpy array and rearrange dimensions
+        flow = predicted_flows[0].cpu().detach().numpy().transpose(1, 2, 0)  # Shape: (H, W, 2)
+
+        return flow
 
 
 def calculate_msen_pepn(gt_flow: np.ndarray, pred_flow: np.ndarray, th: int=3) -> float:
