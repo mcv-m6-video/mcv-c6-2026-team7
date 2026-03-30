0fe10b35c6e7f384c7ec9a879670593aa5b3d2cd
Alejandro Donaire Salvador <113112163+Aledonairesa@users.noreply.github.com>
2026-03-30 20:17:03 +0200

    Add 4 experimental models

---
+++ Week5/model/model_classification_dino.py
@@ -0,0 +1,161 @@
+"""
+File containing the main model.
+"""
+
+#Standard imports
+import torch
+from torch import nn
+import timm
+import torchvision.transforms as T
+from contextlib import nullcontext
+from tqdm import tqdm
+import torch.nn.functional as F
+
+
+#Local imports
+from model.modules import BaseRGBModel, FCLayers, step
+
+class Model(BaseRGBModel):
+ 
+    class Impl(nn.Module):
+ 
+        def __init__(self, args = None):
+            super().__init__()
+            self._feature_arch = args.feature_arch
+ 
+            if self._feature_arch == 'dinov2_small':
+                features = timm.create_model(
+                    'vit_small_patch14_reg4_dinov2.lvd142m',
+                    pretrained=True,
+                    img_size=(224, 398),
+                    num_classes=0  # removes head, returns CLS token features directly
+                )
+                self._d = features.num_features  # 384
+ 
+                # Freeze backbone
+                for param in features.parameters():
+                    param.requires_grad = False
+ 
+            else:
+                raise NotImplementedError(args.feature_arch)
+ 
+            self._features = features
+ 
+            # MLP for classification
+            self._fc = FCLayers(self._d, args.num_classes)
+ 
+            #Augmentations and crop
+            self.augmentation = T.Compose([
+                T.RandomApply([T.ColorJitter(hue = 0.2)], p = 0.25),
+                T.RandomApply([T.ColorJitter(saturation = (0.7, 1.2))], p = 0.25),
+                T.RandomApply([T.ColorJitter(brightness = (0.7, 1.2))], p = 0.25),
+                T.RandomApply([T.ColorJitter(contrast = (0.7, 1.2))], p = 0.25),
+                T.RandomApply([T.GaussianBlur(5)], p = 0.25),
+                T.RandomHorizontalFlip(),
+            ])
+ 
+            #Standarization
+            self.standarization = T.Compose([
+                T.Normalize(mean = (0.485, 0.456, 0.406), std = (0.229, 0.224, 0.225)) #Imagenet mean and std
+            ])
+ 
+        def forward(self, x):
+            x = self.normalize(x) #Normalize to 0-1
+            batch_size, clip_len, channels, height, width = x.shape #B, T, C, H, W
+ 
+            if self.training:
+                x = self.augment(x) #augmentation per-batch
+ 
+            x = self.standarize(x) #standarization imagenet stats
+                        
+            with torch.no_grad():
+                im_feat = self._features(
+                    x.view(-1, channels, height, width)
+                ).reshape(batch_size, clip_len, self._d) #B, T, D
+ 
+            #Max pooling
+            im_feat = torch.max(im_feat, dim=1)[0] #B, D
+ 
+            #MLP
+            im_feat = self._fc(im_feat) #B, num_classes
+ 
+            return im_feat 
+        
+        def normalize(self, x):
+            return x / 255.
+        
+        def augment(self, x):
+            for i in range(x.shape[0]):
+                x[i] = self.augmentation(x[i])
+            return x
+ 
+        def standarize(self, x):
+            for i in range(x.shape[0]):
+                x[i] = self.standarization(x[i])
+            return x
+ 
+        def print_stats(self):
+            print('Model params:',
+                sum(p.numel() for p in self.parameters()))
+ 
+    def __init__(self, args=None):
+        self.device = "cpu"
+        if torch.cuda.is_available() and ("device" in args) and (args.device == "cuda"):
+            self.device = "cuda"
+ 
+        self._model = Model.Impl(args=args)
+        self._model.print_stats()
+        self._args = args
+ 
+        self._model.to(self.device)
+        self._num_classes = args.num_classes
+ 
+    def epoch(self, loader, optimizer=None, scaler=None, lr_scheduler=None):
+ 
+        if optimizer is None:
+            inference = True
+            self._model.eval()
+        else:
+            inference = False
+            optimizer.zero_grad()
+            self._model.train()
+ 
+        epoch_loss = 0.
+        with torch.no_grad() if optimizer is None else nullcontext():
+            for batch_idx, batch in enumerate(tqdm(loader)):
+                frame = batch['frame'].to(self.device).float()
+                label = batch['label']
+                label = label.to(self.device).float()
+ 
+                with torch.cuda.amp.autocast():
+                    pred = self._model(frame)
+                    loss = F.binary_cross_entropy_with_logits(
+                            pred, label)
+ 
+                if optimizer is not None:
+                    step(optimizer, scaler, loss,
+                        lr_scheduler=lr_scheduler)
+ 
+                epoch_loss += loss.detach().item()
+ 
+        return epoch_loss / len(loader)     # Avg loss
+ 
+    def predict(self, seq):
+ 
+        if not isinstance(seq, torch.Tensor):
+            seq = torch.FloatTensor(seq)
+        if len(seq.shape) == 4: # (L, C, H, W)
+            seq = seq.unsqueeze(0)
+        if seq.device != self.device:
+            seq = seq.to(self.device)
+        seq = seq.float()
+ 
+        self._model.eval()
+        with torch.no_grad():
+            with torch.cuda.amp.autocast():
+                pred = self._model(seq)
+ 
+            # apply sigmoid
+            pred = torch.sigmoid(pred)
+            
+            return pred.cpu().numpy()
